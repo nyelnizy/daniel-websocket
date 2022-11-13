31bacc0ace328e758d4568dcd2031a50538be1f4
nyelnizy <yhiamdan@gmail.com>
2022-11-13 19:44:24 +0000

    updates

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Danny Web sockets")
+	startServer()
+}
+
+func startServer() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request)
+		go handleHandShake()
+	})
+	log.Fatal(http.ListenAndServe(":6000", nil))
+}
+
+func handleHandShake() {
+      
+}
+func listenForMessages()  {
+	
 }
